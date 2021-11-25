dea7a9f8b4c788b98ad487c6c034c56cd516d6af
vatran robert <vatranrobert@Airsomtorvatran.lan>
2021-11-25 14:54:41 +0100

    image touch gesture fix

--- Xand0/ViewController.swift
+++ Xand0/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var activePlayer = 1
+    
     
     @IBOutlet var ImageView: UIView!
     
@@ -23,6 +25,21 @@ class ViewController: UIViewController {
    
     @IBAction func TapSquare(_ sender: UITapGestureRecognizer) {
         print("tap")
+        let tappedImage = sender.view as! UIImageView
+        if activePlayer == 1{
+            if let image = UIImage(named: "imageX"){
+                tappedImage.image = image
+                print("image")
+            }
+            
+            activePlayer = 2
+        }else{
+            if let image = UIImage(named: "image0"){
+                tappedImage.image = image
+                print("image")
+            }
+            activePlayer = 1
+        }
         
        
             

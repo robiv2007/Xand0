c03a959d20706777a923e89f6dd12224861b81fa
vatran robert <vatranrobert@Airsomtorvatran.lan>
2021-11-25 12:49:44 +0100

    gesture touch  added

--- Xand0/ViewController.swift
+++ Xand0/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var ImageView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        
+    }
+   
+    @IBAction func TapSquare(_ sender: UITapGestureRecognizer) {
+        print("tap")
+        
+       
+            
+        }
+        
+        
     }
 
+    
 
-}
+
 
